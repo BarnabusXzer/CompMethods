d090b7cc32dd7f0a830fc9f2b8d607ef44aa9872
WHVDevelops <Williamvandyke03@gmail.com>
2022-11-15 22:44:18 -0600

    HW9 Plots

--- HW9/main.py
+++ HW9/main.py
@@ -75,31 +75,55 @@ def calculate():
         ui.txtEquation.setText(f'{coeffs[0]:.2f}x^3 + {coeffs[1]:.2f}x^2 + {coeffs[2]:.2f}x + {coeffs[3]:.2f}')
 
     elif ui.rbtnExp.isChecked():
-        def exponential(args):
+        def exponential(x,args):
             A, B, C = args
             response = A + B * np.e ** (C * x)
             return response
 
-        print(minimize(exponential, [x,1,1,1]))    
+        print(minimize(exponential, (x,[1,1,1])))    
         # ui.txtEquation.setText(str(func))
 
     elif ui.rbtnAll.isChecked():
 
         coeffs = np.polyfit(x,y,1)
         func = np.poly1d(coeffs)
-        plt.plot(x, func(x), 'r', label="Linear Fit")
+        ax = plot.figure.add_subplot(221)
+        ax.scatter(x,y)
+        ax.set_title('Linear Fit')
+        ax.plot(x, func(x), 'r')
+        ax.axes.set_ylabel('Y Label')
+        ax.axes.set_xlabel('X Label')
+        plot.draw()
 
         coeffs = np.polyfit(x,y,2)
         func = np.poly1d(coeffs)
-        plt.plot(x, func(x), 'g', label="Quadratic Fit")
+        ax = plot.figure.add_subplot(222)
+        ax.scatter(x,y)
+        ax.set_title('Quadratic Fit')
+        ax.plot(x, func(x), 'r')
+        ax.axes.set_ylabel('Y Label')
+        ax.axes.set_xlabel('X Label')
+        plot.draw()
 
         coeffs = np.polyfit(x,y,3)
         func = np.poly1d(coeffs)
-        plt.plot(x, func(x), 'b', label="Cubic Fit")
+        ax = plot.figure.add_subplot(223)
+        ax.scatter(x,y)
+        ax.set_title('Cubic Fit')
+        ax.plot(x, func(x), 'r')
+        ax.axes.set_ylabel('Y Label')
+        ax.axes.set_xlabel('X Label')
+        plot.draw()
 
         coeffs = np.polyfit(x,y,3)
         func = np.poly1d(coeffs)
-        plt.plot(x, func(x), 'y', label="Exponential Fit")
+        ax = plot.figure.add_subplot(224)
+        ax.scatter(x,y)
+        ax.set_title('Exponential Fit')
+        ax.plot(x, func(x), 'r')
+        ax.axes.set_ylabel('Y Label')
+        ax.axes.set_xlabel('X Label')
+        plot.draw()
 
 if __name__ == "__main__":
     Window1.show()
